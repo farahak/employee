bcc202fa99f858d79c563f8550e5607d9394a20d
farahak <farahakrout33@gmail.com>
2024-05-21 11:13:05 +0200

    employee

---
+++ employeeManagementSystem/src/employeemanagementsystem/database.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Farah et Maryem
+ 
+ *
+ */
+public class database {
+    
+    public static Connection connectDb(){
+        
+        try{
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/employee", "root", "");
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
+    
+}
---
+++ employeeManagementSystem/src/employeemanagementsystem/getData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagementsystem;
+
+/**
+ *
+ * @author maryem et farah
+ * 
+ */
+public class getData {
+    public static String username;
+    public static String path;
+}
